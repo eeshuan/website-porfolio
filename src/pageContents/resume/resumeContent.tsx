445bf35a26cb585c9955a7df2cf89c168aa926a5
eeshuan <ngeeshuan@hotmail.com>
2019-03-19 22:19:46 +0800

    Updated resume page

---
+++ src/pageContents/resume/resumeContent.tsx
@@ -0,0 +1,104 @@
+export let resumeContent = {
+    skills: {
+        programming: [
+            "ReactJS and React-Native",
+            "HTML, Webpack",
+            "CSS, SASS/SCSS",
+            "GIT, IDE, NodeJS, MongoDB",
+            "JavaScript, TypeScript",
+            "C, C++, C#, Java"
+        ],
+        gameDev: [
+            "Unreal Engine 4, Unity 2D/3D",
+            "Cocos2d-x, Cocos Creator",
+            "PixiJS, PhaserJS"
+        ],
+        others: [
+            "Telecom Skills",
+            "Customer Service",
+            "Event Management"
+        ]
+    },
+    work: [
+        {
+            company: "Autumn Interactive Pte Ltd, Singapore",
+            position: "Software Developer",
+            duration: "May 2018 - PRESENT",
+            details: [
+                "Led the team in the creation of mini games for the company",
+                "In charge of maximizing the UI/UX experience for the products",
+                "In charge of the software testing for the products",
+                "Responsible for managing all live products of the project"
+            ]
+        },
+        {
+            company: "Autumn Interactive Pte Ltd, Singapore",
+            position: "Part-time Developer",
+            duration: "December 2017 - April 2018",
+            details: [
+                "Helped the team to create mini games for the company",
+                "Helped out in maximizing the UI/UX experience of softwares",
+                "Helped out engaging in software testing"
+            ]
+        },
+        {
+            company: "HiVelocity Events Pte Ltd, Singapore ",
+            position: "IT Admin Intern",
+            duration: "May 2017 - October 2017",
+            details: [
+                "In charge of all IT-related events of the company",
+                "Learnt how to manage  business servers and networks",
+                "Was in charge of setting up an off-site database for offline data entry",
+                "Learnt and helped in the company’s web and software development"
+            ]
+        },
+        {
+            company: "Sushi Tei Pte Ltd, Singapore",
+            position: "Part-time Service Staff",
+            duration: "July 2016 - January 2017",
+            details: [
+                "Worked as a service staff (Waiter)",
+                "Learnt how to handle difficult customers with professionalism"
+            ]
+        },
+        {
+            company: "Singtel, Singapore",
+            position: "Part-time Telecom Operator",
+            duration: "April 2014 - June 2014",
+            details: [
+                "Managed incoming call queries from customers",
+                "Helped to handle and make sales orders for customers that call in"
+            ]
+        },
+        {
+            company: "Constant Wind Pte Ltd, Singapore",
+            position: "Part-time Service Staff",
+            duration: "January 2012 - March 2012",
+            details: [
+                "Helped out in the customer service and retail sectors"
+            ]
+        }
+    ],
+    education: [
+        {
+            school: "National University of Singapore, Singapore",
+            cert: "Bachelor of Computing (Computer Science)",
+            duration: "August 2014 - June 2018",
+            details: [
+                "Graduated from the School of Computing with Merit",
+                "Obtained high distinction in all game development subjects",
+                "Participated in NUS STePS events in 2018 and won 2nd place",
+                "Participated in NUS Orbital Project in 2015"
+            ]
+        },
+        {
+            school: "Meridian Junior College, Singapore ",
+            cert: "GCE ‘A’ Levels",
+            duration: "January 2010 - November 2011",
+            details: [
+                "Graduated with 85 rank points",
+                "Obtained distinction in all Sciences and Maths subject fields"
+            ]
+        }
+    ]
+}
--- src/pages/resume/resume.tsx
+++ src/pages/resume/resume.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Layout from '../layout/layout';
 import ImageCard from '../../components/imageCard/imageCard';
 import { assetsMap } from '../../assets/assetsMap';
+import { Grid } from '@material-ui/core';
+import { resumeContent } from '../../pageContents/resume/resumeContent';
 
 import './resume.scss';
 
@@ -19,8 +21,140 @@ export default class Resume extends React.Component {
                         subtitle="Reume here!"
                         image={assetsMap.pages.portfolio}
                     />
-                    <div>
-                        I am a programmer
+                    <div className="resume-container container-even">
+                        <div className="resume-title">
+                            Skills
+                        </div>
+                        <div className="resume-content">
+                            <Grid container alignItems="flex-start" justify="flex-start">
+                                <Grid xs={12} sm={6} item>
+                                    <div className="skills-title">
+                                        Programming
+                                    </div>
+                                    <div className="skills-info">
+                                        <ul>
+                                            {
+                                                resumeContent.skills.programming.map((data: any, index: number)=>{
+                                                    return (
+                                                        <li key={index}>{data}</li>
+                                                    );
+                                                })
+                                            }
+                                        </ul>
+                                    </div>
+                                </Grid>
+                                <Grid xs={12} sm={6} item>
+                                    <div className="skills-title">
+                                        Game Development
+                                    </div>
+                                    <div className="skills-info">
+                                        <ul>
+                                            {
+                                                resumeContent.skills.gameDev.map((data: any, index: number)=>{
+                                                    return (
+                                                        <li key={index}>{data}</li>
+                                                    );
+                                                })
+                                            }
+                                        </ul>
+                                    </div>
+                                </Grid>
+                                <Grid xs={12} sm={6} item>
+                                    <div className="skills-title">
+                                        Others
+                                    </div>
+                                    <div className="skills-info">
+                                        <ul>
+                                            {
+                                                resumeContent.skills.others.map((data: any, index: number)=>{
+                                                    return (
+                                                        <li key={index}>{data}</li>
+                                                    );
+                                                })
+                                            }
+                                        </ul>
+                                    </div>
+                                </Grid>
+                            </Grid>
+                        </div>
+                    </div>
+                    <div className="resume-container container-odd">
+                        <div className="resume-title">
+                            Work Experiences
+                        </div>
+                        <div className="resume-content">
+                            {
+                                resumeContent.work.map((data: any, index: number)=>{
+                                    return (
+                                        <Grid container alignItems="flex-start" justify="flex-start" key={index} className="experience-container">
+                                            <Grid xs={12} sm={6} item>
+                                                <div className="experience-company">
+                                                    {data.company}
+                                                </div>
+                                                <div className="experience-position">
+                                                    <i>{data.position}</i>
+                                                </div>
+                                                <div className="experience-year">
+                                                    <i>{data.duration}</i>
+                                                </div>
+                                            </Grid>
+                                            <Grid xs={12} sm={6} item>
+                                                <div>
+                                                    <ul>
+                                                        {
+                                                            data.details.map((listData: any, listIndex: number)=>{
+                                                                return (
+                                                                    <li key={listIndex}>{listData}</li>
+                                                                );
+                                                            })
+                                                        }
+                                                    </ul>
+                                                </div>
+                                            </Grid>
+                                        </Grid>
+                                    );
+                                })   
+                            }
+                        </div>
+                    </div>
+                    <div className="resume-container container-even">
+                        <div className="resume-title">
+                            Education
+                        </div>
+                        <div className="resume-content">
+                            {
+                                resumeContent.education.map((data: any, index: number)=>{
+                                    return (
+                                        <Grid container alignItems="flex-start" justify="flex-start" key={index} className="education-container">
+                                            <Grid xs={12} sm={6} item>
+                                                <div className="education-school">
+                                                    {data.school}
+                                                </div>
+                                                <div className="education-cert">
+                                                    <i>{data.cert}</i>
+                                                </div>
+                                                <div className="education-year">
+                                                    <i>{data.duration}</i>
+                                                </div>
+                                            </Grid>
+                                            <Grid xs={12} sm={6} item>
+                                                <div>
+                                                    <ul>
+                                                        {
+                                                            data.details.map((listData: any, listIndex: number)=>{
+                                                                return (
+                                                                    <li key={listIndex}>{listData}</li>
+                                                                );
+                                                            })
+                                                        }
+                                                    </ul>
+                                                </div>
+                                            </Grid>
+                                        </Grid>
+                                    );
+                                })   
+                            }
+                        </div>
                     </div>
                 </div>
             </Layout>
