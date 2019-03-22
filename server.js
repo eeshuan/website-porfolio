0cd008d7de789fad50afbf4f0bed59a16db5b4f8
eeshuan <ngeeshuan@hotmail.com>
2019-03-23 01:22:47 +0800

    Optimized assets?

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 let path = require("path");
 
-let port = 8000;
+let port = 80;
 
 app.use(express.static(path.join(process.cwd(), 'dist')));
 
